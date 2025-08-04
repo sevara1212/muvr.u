9bf90036c766499443fb4db1094597a5616444ed
sevara1212 <sibr0471@uni.sydney.edu.au>
2025-08-04 18:56:43 +1000

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
-import { useTelegramViewport } from "@/hooks/useTelegramViewport";
 import Index from "./pages/Index";
 import ExplorePage from "./pages/ExplorePage";
 import CreateRoomPage from "./pages/CreateRoomPage";
@@ -22,29 +21,6 @@ import ChatPage from "./pages/ChatPage";
 
 const queryClient = new QueryClient();
 
-const AppContent = () => {
-  useTelegramViewport(); // Apply Telegram viewport fix
-  
-  return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/explore" element={<ExplorePage />} />
-      <Route path="/create" element={<CreateRoomPage />} />
-      <Route path="/room/:id" element={<RoomDetailPage />} />
-      <Route path="/join/:id" element={<RoomDetailPage />} />
-      <Route path="/edit-room/:id" element={<EditRoomPage />} />
-      <Route path="/activities" element={<ActivitiesPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/payment" element={<PaymentPage />} />
-      <Route path="/category/:sport" element={<ExplorePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/chat/:roomId" element={<ChatPage />} />
-      <Route path="*" element={<NotFoundWithLayout />} />
-    </Routes>
-  );
-};
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
@@ -52,7 +28,22 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <AppContent />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/explore" element={<ExplorePage />} />
+            <Route path="/create" element={<CreateRoomPage />} />
+            <Route path="/room/:id" element={<RoomDetailPage />} />
+            <Route path="/join/:id" element={<RoomDetailPage />} />
+            <Route path="/edit-room/:id" element={<EditRoomPage />} />
+            <Route path="/activities" element={<ActivitiesPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/payment" element={<PaymentPage />} />
+            <Route path="/category/:sport" element={<ExplorePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/chat/:roomId" element={<ChatPage />} />
+            <Route path="*" element={<NotFoundWithLayout />} />
+          </Routes>
         </BrowserRouter>
       </TooltipProvider>
     </AuthProvider>
